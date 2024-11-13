f89dbd7f4443009b2a650d2a932dfbab2b661788
daniel-kinde <daniel@kinde.com>
2024-11-13 22:14:21 +0000

    Update extendAccessTokenWorkflow.ts

--- kindeSrc/environment/workflows/extendAccessTokenWorkflow.ts
+++ kindeSrc/environment/workflows/extendAccessTokenWorkflow.ts
@@ -32,6 +32,6 @@ export default {
     const accessToken = accessTokenCustomClaims<{ hello: string; settings: string; permissions: []}>();
     accessToken.hello = "Hello there!";
     accessToken.settings = settings.output
-    accessToken.permissions =  res.json.permissions.filter((p) => !excludedPermissions.includes(p.key))
+    accessToken.permissions =  res.result.json.permissions.filter((p) => !excludedPermissions.includes(p.key))
   }
 }
